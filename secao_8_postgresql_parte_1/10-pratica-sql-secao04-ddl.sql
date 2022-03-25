53a48108af74a9e68bebf09da55b19867a5c27fb
tiagovianez <tiagovianez@live.com>
2022-03-25 19:25:11 -0300

    DDL

--- secao_8_postgresql_parte_1/10-pratica-sql-secao04-ddl.sql
+++ secao_8_postgresql_parte_1/10-pratica-sql-secao04-ddl.sql
@@ -1,30 +1,28 @@
 -- Create
--- CREATE DATABASE secao04;
+CREATE DATABASE secao04;
 
 -- Create
--- CREATE TABLE tipos_produto(
---	codigo SERIAL PRIMARY KEY,
---	descricao VARCHAR(30) NOT NULL
--- );
+CREATE TABLE tipos_produto(
+	codigo SERIAL PRIMARY KEY,
+	descricao VARCHAR(30) NOT NULL
+);
 
 -- Create
--- CREATE TABLE produtos(
---	codigo SERIAL PRIMARY KEY, 
---	descricao VARCHAR(30) NOT NULL,
---	preco MONEY NOT NULL,
---	codigo_tipo INT REFERENCES tipos_produto(codigo) NOT NULL
--- );
+CREATE TABLE produtos(
+	codigo SERIAL PRIMARY KEY, 
+	descricao VARCHAR(30) NOT NULL,
+	preco MONEY NOT NULL,
+	codigo_tipo INT REFERENCES tipos_produto(codigo) NOT NULL
+);
 
 -- Alter
--- ALTER TABLE tipos_produto ADD peso DECIMAL(8,2);
+ALTER TABLE tipos_produto ADD peso DECIMAL(8,2);
 
 -- Verificando se o novo campo foi realmente adicionado
--- SELECT * FROM tipos_produto;
-
+SELECT * FROM tipos_produto;
 
 -- Drop (Não irá permitir deletar a tabel apor conter um relacionamento com produdos)
--- DROP TABLE tipos_produto;
-
+DROP TABLE tipos_produto;
 
 -- mas podemos deletar todo o banco de dados
 DROP DATABASE secao03;
