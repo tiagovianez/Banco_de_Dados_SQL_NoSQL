2bd4d3083d393e9323589b63f87f611667605221
tiagovianez <tiagovianez@live.com>
2022-03-25 19:26:53 -0300

    Project Test II

--- secao_8_postgresql_parte_1/14-pratica-sql-secao05-parte2.sql
+++ secao_8_postgresql_parte_1/14-pratica-sql-secao05-parte2.sql
@@ -1,6 +1,6 @@
--- SELECT * FROM tipos_produto;
+SELECT * FROM tipos_produto;
 
--- SELECT * FROM produtos;
+SELECT * FROM produtos;
 
 SELECT p.codigo, p.descricao, p.preco, tp.descricao AS Tipo
 	FROM produtos AS p, tipos_produto AS tp
