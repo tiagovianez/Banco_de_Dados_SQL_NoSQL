708cb5dd1b404932e69c4e782cc58a45abe85958
tiagovianez <tiagovianez@live.com>
2022-03-25 19:24:20 -0300

    Inserção de Dados II

--- secao_8_postgresql_parte_1/07-pratica-sql-secao03-parte2.sql
+++ secao_8_postgresql_parte_1/07-pratica-sql-secao03-parte2.sql
@@ -1,14 +1,14 @@
 -- INSERTS
 
 -- Tipos Produto
--- INSERT INTO tipos_produto (descricao) VALUES ('Computadores');
--- INSERT INTO tipos_produto (descricao) VALUES ('Impressoras');
--- INSERT INTO tipos_produto (descricao) VALUES ('Diversos');
+INSERT INTO tipos_produto (descricao) VALUES ('Computadores');
+INSERT INTO tipos_produto (descricao) VALUES ('Impressoras');
+INSERT INTO tipos_produto (descricao) VALUES ('Diversos');
 
 -- Produtos
--- INSERT INTO produtos (descricao, preco, id_tipo_produto) VALUES ('Notebook DELL 1544', 2345.67, 1);
--- INSERT INTO produtos (descricao, preco, id_tipo_produto) VALUES ('Impr. Jato de Tinta', 456.00, 2);
--- INSERT INTO produtos (descricao, preco, id_tipo_produto) VALUES ('Mouse sem fio', 45, 3);
+INSERT INTO produtos (descricao, preco, id_tipo_produto) VALUES ('Notebook DELL 1544', 2345.67, 1);
+INSERT INTO produtos (descricao, preco, id_tipo_produto) VALUES ('Impr. Jato de Tinta', 456.00, 2);
+INSERT INTO produtos (descricao, preco, id_tipo_produto) VALUES ('Mouse sem fio', 45, 3);
 
 -- Pacientes
 INSERT INTO pacientes (nome, endereco, bairro, cidade, estado, cep, data_nascimento) 
