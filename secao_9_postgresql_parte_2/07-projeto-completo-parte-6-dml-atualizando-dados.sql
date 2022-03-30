03e80771b985553047b2ed0da12d95ddd1e73510
tiagovianez <tiagovianez@live.com>
2022-03-30 18:33:15 -0300

    Atualizando Dados

---
+++ secao_9_postgresql_parte_2/07-projeto-completo-parte-6-dml-atualizando-dados.sql
@@ -0,0 +1,8 @@
+SELECT id, nome FROM atores ORDER BY id ASC;
+
+UPDATE atores SET nome = 'Bread Pitt Silva' WHERE id = 1;
+
+SELECT * FROM emprestimos;
+
+UPDATE emprestimos SET data = '2022-03-25' WHERE id = 3;
+
